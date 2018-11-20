79500dc55feed05de777e3c7b3e36d84a58b2c0a
Nanra <sukedynanra@gmail.com>
2018-11-21 02:08:07 +0700

    Add Models on Blog Apps

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blog # Import Model yang mau di daftarkan di laman admin
 
 # Register your models here.
+admin.site.register(Blog) # Mendaftarkan class Blog dari Models
