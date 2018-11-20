ee9d5daa24c068b4f48cefe630399d13b8206357
Nanra <sukedynanra@gmail.com>
2018-11-20 23:42:44 +0700

    Add migration and Job Models

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Job # Import Model yang mau di daftarkan di laman admin
 
 # Register your models here.
+admin.site.register(Job) # Mendaftarkan class Job dari Models
