fee8feb7236abf77d7de67cd176135201820dec5
Nanra <sukedynanra@gmail.com>
2018-11-22 03:14:20 +0700

    Show Jobs in Home Views

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Job # Memanggil Model Job pada database
 
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+    isiJobs = Job.objects # Memanggil semua isi dari model Job pada database
+    return render(request, 'jobs/home.html', {'jobs': isiJobs})
