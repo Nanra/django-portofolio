c0c501911c8f970244c4160ec49057861130c066
Nanra <sukedynanra@gmail.com>
2018-11-22 04:11:26 +0700

    Show Blog Post in Blog Home Page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def listBlog(request):
-    return render(request, 'blog/list-blog.html')
+    isiBlog = Blog.objects
+    return render(request, 'blog/list-blog.html', {'blogs':isiBlog})
