aafd5e20690640abe7e9ed7aa7d0366fc5759d33
Nanra <sukedynanra@gmail.com>
2018-11-22 05:08:05 +0700

    :art: Add Details Page for View Detail Post :sparkles:

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -6,5 +6,6 @@ def listBlog(request):
     isiBlog = Blog.objects
     return render(request, 'blog/list-blog.html', {'blogs':isiBlog})
 
-def detail(request):
-    return render(request, 'blog/detail.html')
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'isiDetail':detailBlog})
